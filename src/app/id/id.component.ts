dfbbe911c2e1d93520458d65df68812c12fad072
Anitha Madanasekar <anithama@virtusa.com>
2018-06-20 18:45:49 +0530

    Partially completed

--- src/app/id/id.component.ts
+++ src/app/id/id.component.ts
@@ -1,3 +1,7 @@
+import { Http } from '@angular/http';
+import { AbstractControl } from '@angular/forms';
+import { ValidationErrors } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,13 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./id.component.css']
 })
 export class IdComponent implements OnInit {
-idFormGroup:FormGroup;
-  constructor() { 
-    this.idFormGroup=new this.idFormGroup
-    firstName:new HTMLFormControlsCollection(' ',validators.required)
-  }
+ idFormGroup:FormGroup;
+   constructor(private httpService: Http) { 
+     this.idFormGroup=new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      
+     })
+//     firstName:new HTMLFormControlsCollection(' ',validators.required)
+   }
 
   ngOnInit() {
+  }
+  onSubmit() {
+    console.log(this.idFormGroup.value);
+
+    const postData = {
+
+    }
+    const url = 'https://id-application-form.firebaseio.com/applicationIds.json';
+    this.httpService.post(url, this.idFormGroup.value)
+      .subscribe(rsp => console.log(rsp));
+    console.log('Posted');
   }
 
 }
